41d76e0e17bde6076cdc381a878258c7b09e9ede
developer-shkim <developer.shkim@gmail.com>
2022-03-02 00:44:54 +0900

    임시로 appleSauce 함수로 분리

--- app/chapter01/statement.js
+++ app/chapter01/statement.js
@@ -3,6 +3,16 @@ export default function statement(invoice, plays) {
     return plays[aPerformance.playID];
   }
 
+  function appleSauce() {
+    let totalAmount = 0;
+    
+    for (let perf of invoice.performances) {
+      totalAmount += amountFor(perf);
+    }
+
+    return totalAmount;
+  }
+
   function totalVolumeCredits() {
     let volumeCredits = 0;
     for (let perf of invoice.performances) {
@@ -56,14 +66,12 @@ export default function statement(invoice, plays) {
     return result;
   }
 
-  let totalAmount = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-
   for (let perf of invoice.performances) {
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-    totalAmount += amountFor(perf);
   }
   
+  let totalAmount = appleSauce();
   result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n\n`;
 
