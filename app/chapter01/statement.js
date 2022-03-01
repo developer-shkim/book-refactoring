fcb400471d2103c184beb5e3e83065e87737456f
developer-shkim <developer.shkim@gmail.com>
2022-03-02 01:03:02 +0900

    playFor 옮기기

--- app/chapter01/statement.js
+++ app/chapter01/statement.js
@@ -1,16 +1,22 @@
 export default function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances;
+  statementData.performances = invoice.performances.map(enrichPerformance);
+
+  function enrichPerformance(aPerformance) {
+    const result = Object.assign({}, aPerformance);
+    result.play = playFor(result);
+    return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
 
   return renderPlainText(statementData, plays);
 }
 
 function renderPlainText(data, plays) {
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
   function totalAmount() {
     let result = 0;
 
@@ -42,7 +48,7 @@ function renderPlainText(data, plays) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
 
-    if ("comedy" === playFor(aPerformance).type)
+    if ("comedy" === aPerformance.play.type)
     result += Math.floor(aPerformance.audience / 5);
 
     return result;
@@ -51,7 +57,7 @@ function renderPlainText(data, plays) {
   function amountFor(aPerformance) {
     let result = 0;
 
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -68,7 +74,7 @@ function renderPlainText(data, plays) {
         break;
 
       default:
-        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
     }
 
     return result;
@@ -76,7 +82,7 @@ function renderPlainText(data, plays) {
 
   let result = `청구 내역 (고객명: ${data.customer})\n`;
   for (let perf of data.performances) {
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+    result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
   }
   
   result += `총액: ${usd(totalAmount())}\n`;
