700b8543e5f46e1bfaa38d53b26d929309416e11
developer-shkim <developer.shkim@gmail.com>
2022-03-02 00:55:19 +0900

    statement 로직을 두 단계로 나누기

--- app/chapter01/statement.js
+++ app/chapter01/statement.js
@@ -1,4 +1,12 @@
 export default function statement(invoice, plays) {
+  const statementData = {};
+  statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances;
+
+  return renderPlainText(statementData, plays);
+}
+
+function renderPlainText(data, plays) {
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
@@ -6,7 +14,7 @@ export default function statement(invoice, plays) {
   function totalAmount() {
     let result = 0;
 
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += amountFor(perf);
     }
 
@@ -15,7 +23,7 @@ export default function statement(invoice, plays) {
 
   function totalVolumeCredits() {
     let volumeCredits = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       volumeCredits += volumeCreditsFor(perf);
     }
 
@@ -66,8 +74,8 @@ export default function statement(invoice, plays) {
     return result;
   }
 
-  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-  for (let perf of invoice.performances) {
+  let result = `청구 내역 (고객명: ${data.customer})\n`;
+  for (let perf of data.performances) {
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
   }
   
